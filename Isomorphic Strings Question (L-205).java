35cf23920e07ceb78d0f44d7772faef8589340e6
riteshkhamitkar <89683381+riteshkhamitkar@users.noreply.github.com>
2022-07-26 11:28:35 +0530

    Add files via upload

---
+++ Isomorphic Strings Question (L-205).java
@@ -0,0 +1,19 @@
+class Solution {
+    public boolean isIsomorphic(String s, String t) {
+        char [] sToT = new char[256];
+        char[] tToS = new char[256];
+        
+        for(int i = 0; i < s.length(); i++){
+            char sChar = s.charAt(i);
+            char tChar = t.charAt(i);
+            
+            if  (sToT[sChar]==0 && tToS[tChar]==0){
+                sToT[sChar] = tChar;
+                tToS[tChar] = sChar;
+            }else if(sToT[sChar] != tChar){
+                return false;
+            }
+        }
+        return true;
+    }
+}
